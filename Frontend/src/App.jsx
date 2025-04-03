2ffc0d12ff68701d0c54a85d98b77f3b08b2e515
Sheetu1 <sheetusharma548@gmail.com>
2025-04-03 18:30:35 +0530

    FullStack Learning management system first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+import { Auth } from "./pages/Auth/Auth";
+import { CardWithForm } from "./pages/Card/Card";
+
+function App() {
+  return (
+    <>
+      <Auth/>
+    </>
+  );
+}
+
+export default App;
+
+// NMKJfqSkqeCNJEUe password
+// mongodb+srv://sheetusharma548:NMKJfqSkqeCNJEUe@cluster0.dataq.mongodb.net/ (url)
